d7f3186e3e3bf0a2d6c9683201d64228bac11782
Ellmor <nezi2007@gmail.com>
2015-12-01 11:32:21 +0100

    Update

--- MySensei.Entities/Tag.cs
+++ MySensei.Entities/Tag.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Data.Entity.Spatial;
-namespace MySensei.Models
+namespace MySensei.Entities
 {
     public class Tag
     {
